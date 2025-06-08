338c7e4a5e3959c184a6aab6ead54b598b41d2cf
astitvabhate <bhateastitva204@gmail.com>
2025-06-08 17:53:54 +0530

    .

--- Backend/server.js
+++ Backend/server.js
@@ -33,7 +33,7 @@ const jobRoutes = require('../Backend/routes/job_routes.js');
 app.use('/api/jobs', jobRoutes);
 
 // Server
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
